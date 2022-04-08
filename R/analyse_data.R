2fabf3143f6e0e67ff60536cf19350eba7b3ae96
Taren Sanders <taren.sanders@acu.edu.au>
2022-04-08 15:18:38 +1000

    First pass of analysis

--- R/analyse_data.R
+++ R/analyse_data.R
@@ -8,7 +8,13 @@
 #' @author Taren Sanders
 #' @export
 analyse_data <- function(df_clean) {
+  model_unadj <- lmerTest::lmer(cwb_who_total ~
+  intervention * time_point * cwb_who_total_f +
+    (1 | cid_id) + (1 | sid_id), data = df_clean)
 
-  NULL
-
+  model_adj <- lmerTest::lmer(cwb_who_total ~
+  intervention * time_point * cwb_who_total_f +
+    cdem_age + cdem_atsi + cdem_books + cdem_lang +
+    cdem_sex + cdem_wealth + sdem_icsea + sdem_remote +
+    (1 | cid_id) + (1 | sid_id), data = df_clean)
 }
