ef6d561f3e8cdfe554f0c1ade3ef9fe94c7e6bab
Taren Sanders <taren.sanders@acu.edu.au>
2022-04-13 09:34:40 +1000

    Fix missing data

--- R/analyse_data.R
+++ R/analyse_data.R
@@ -8,6 +8,10 @@
 #' @author Taren Sanders
 #' @export
 analyse_data <- function(df_clean) {
+  model_overall <-
+    lmerTest::lmer(cwb_who_total ~ intervention * time_point +
+      (1 | cid_id) + (1 | sid_id), data = df_clean)
+
   model_unadj <-
     lmerTest::lmer(cwb_who_total ~ intervention * time_point * cwb_who_total_f +
       (1 | cid_id) + (1 | sid_id), data = df_clean)
@@ -15,10 +19,13 @@ analyse_data <- function(df_clean) {
   model_adj <-
     lmerTest::lmer(cwb_who_total ~ intervention * time_point * cwb_who_total_f +
       cdem_age + cdem_atsi + cdem_books + cdem_lang + cdem_sex + cdem_wealth +
-      sdem_icsea + sdem_remote + (1 | cid_id) + (1 | sid_id), data = df_clean)
+      sdem_icsea + sdem_remote + (1 | cid_id) + (1 | sid_id),
+    data = df_clean %>% tidyr::drop_na()
+    )
 
   return(list(
     "unadjusted" = model_unadj,
-    "adjusted" = model_adj
+    "adjusted" = model_adj,
+    "overall" = model_overall
   ))
 }
--- R/clean_data.R
+++ R/clean_data.R
@@ -9,12 +9,32 @@
 #' @export
 clean_data <- function(df_raw) {
   df_clean <- df_raw %>%
-    dplyr::group_by(cid_id) %>%
-    dplyr::mutate(cwb_who_total_f = dplyr::first(cwb_who_total)) %>%
+    dplyr::mutate(
+      time_point = factor(
+        time_point,
+        levels = c(0, 1, 2),
+        labels = c("Baseline", "12m", "24m")
+      )
+    ) %>%
+    # Fix missing data
+    group_by(time_point) %>%
+    mutate(
+      cdem_age = if_else(is.na(cdem_age), median(cdem_age), cdem_age),
+      cdem_atsi = forcats:::fct_explicit_na(cdem_atsi, "Don't know")
+    ) %>%
     dplyr::ungroup() %>%
-    tidyr::drop_na()
-
-  # TODO: Fix the missing data
+    dplyr::group_by(cid_id) %>%
+    dplyr::mutate(
+      cwb_who_total_f = dplyr::first(cwb_who_total),
+      across(c(
+        cdem_age, cdem_atsi, cdem_atsi, cdem_bornaus, cdem_lang, cdem_sex,
+        cdem_wealth
+      ), dplyr::first)
+      # Covariates should be set to baseline
+    ) %>%
+    dplyr::ungroup() %>%
+    # Exclude those without any outcome data
+    tidyr::drop_na(cwb_who_total)
 
   return(df_clean)
 }
